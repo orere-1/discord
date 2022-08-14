86ec65e84a68802bf7b1ae45803192f084cf7e40
orere-1 <109005905+orere-1@users.noreply.github.com>
2022-08-14 11:13:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import urllib.request
 import json
 import os
+import asyncio
 
 token = 'OTY3NTg4NzczMDQ5Mjk4OTU1.GwP35_.sgClt4SBQHY8lslLvSKEsuz9wsLh9Huvcnn8KA'
 messageId = 990415099468603462
